07510955d2d2b34d2b66f44dd81e5fe4d9f666f9
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-02-08 22:12:37 -0500

    Ternary Operators, Lists in React, Css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import "./App.css";
 
 function App() {
+  const cname = 2;
+  const isGreen = true;
   return (
-    <div className="App">
-      Hello
+    <>
+      <div className="App Extracss">
+        Hello
+        <br />
+        <User name="Aishwarya" age={24} />
+        <Job position="Web Developer" salary={1700000} company="Google" />
+        <br />
+        <User name="Apoorva" age={24} />
+        <Job position="Software Developer" salary={1200000} company="IBM" />
+      </div>
       <br />
-      <User name="Aishwarya" age={24} />
-      <Job position="Web Developer" salary={1700000} company="Google" />
-      <br />
-      <User name="Apoorva" age={24} />
-      <Job position="Software Developer" salary={1200000} company="IBM" />
-    </div>
+      <div className="App Extracss">
+        {cname >= 2 ? <h2>CLASS A</h2> : <h2>CLASS B</h2>}
+        <h1 style={{ color: isGreen ? "green" : "red" }}>
+          Testing the isGreen.
+        </h1>
+        {isGreen && <button>Yes</button>}
+      </div>
+    </>
   );
 }
 
