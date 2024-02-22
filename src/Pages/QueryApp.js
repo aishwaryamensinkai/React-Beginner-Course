cb0ac71f184843f3cdc5f8846eeae069019df22a
Aishwarya Mensinkai <aishwaryamensinkai@gmail.com>
2024-02-22 11:55:42 -0500

    React-Query| How to Properly Fetch Data in React

--- src/Pages/QueryApp.js
+++ src/Pages/QueryApp.js
@@ -1,12 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 export const QueryApp = () => {
-  const { data } = useQuery(["cat"], () => {
-    fetch("https://catfact.ninja/fact")
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data);
-        return data.fact;
-      });
+  const { isPending, data, error } = useQuery({
+    queryKey: ["cat"],
+    queryFn: () =>
+      fetch("https://catfact.ninja/fact")
+        .then((res) => res.json())
+        .then((data) => {
+          return data;
+        }),
   });
-  return <h1>{data}</h1>;
+
+  if (error) {
+    return <h1> Sorry, there was an error {error.message} </h1>;
+  }
+
+  if (isPending) {
+    return <h1> Loading...</h1>;
+  }
+
+  return (
+    <h1>
+      <p>{data.fact}</p>
+    </h1>
+  );
 };
